5b7e32ea3ea2e84438a22f2aae9cff059dd792c5
oldseven <oldseven@nn.com>
2017-03-20 17:59:01 +0800

    fix environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,13 @@
 # Load the Rails application.
 require_relative 'application'
 
-env_file = Rails.root.join("config", 'environments.yml').to_s
+# env_file = Rails.root.join("config", 'environments.yml').to_s
 
-if File.exists?(env_file) && (env_hash = YAML.load_file(env_file)[Rails.env]).present?
-  env_hash.each do |key, value|
-    ENV[key.to_s] = value
-  end
-end
+# if File.exists?(env_file) && (env_hash = YAML.load_file(env_file)[Rails.env]).present?
+#   env_hash.each do |key, value|
+#     ENV[key.to_s] = value
+#   end
+# end
 
 # Initialize the Rails application.
 Rails.application.initialize!
