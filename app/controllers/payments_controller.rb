692b5b2d7d416e3242c4b555a8839b7bd87ce8ec
oldseven <oldseven@nn.com>
2017-03-22 10:02:55 +0800

    remove binding.pry

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -14,7 +14,6 @@ class PaymentsController < ApplicationController
  #  					  'X-Nonce' => nonce,
  #  					  'X-Client' => 1 })
   def initialize
-    binding.pry
     @client = StraightServerKit::Client.new(gateway_id: 1, secret: ENV['STRAIGHT_SERVER_SECRET'], url: 'http://localhost:9696/')
     super
   end
