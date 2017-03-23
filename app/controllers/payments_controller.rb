7a25ce2191d843790f8b5dd93a0176330897415e
oldseven <oldseven@nn.com>
2017-03-23 11:57:04 +0800

    modify css

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,18 +7,18 @@ class PaymentsController < ApplicationController
   end
 
   def index
-  end
-
-  def invoice
+    if params[:uid].blank?
+      request.referer.blank? ? (return render plain: "ERROR: lack of params.") : (return redirect_to request.referer)
+    end
   end
 
   def update
   	begin
 	  	@client.orders.cancel(id: params[:id])
-	  	return redirect_to :index
-	rescue
-		return render :failed
-	end
+	  	return redirect_to :index, locals: { uid: params[:uid] }
+  	rescue
+  		return render :failed, locals: { error_info: "ERROR: This order cannot be canceled now. Please try again later." }
+  	end
   end
 
   def create
@@ -26,17 +26,17 @@ class PaymentsController < ApplicationController
       # res = HTTParty.get(ENV['STRAIGHT_SERVER_URL'] + 'gateways/1/last_keychain_id')
       res = Excon.get(ENV['STRAIGHT_SERVER_URL'] + 'gateways/1/last_keychain_id')[:body]
       last_keychain_id = JSON.parse(res)['last_keychain_id'].to_i
-    rescue
-      return render plain: "ERROR: can't connect to straight server"
-    end
-  	if params[:amount].blank? || params[:user_uid].blank?
-      request.referer.blank? ? (return render plain: "ERROR: lack of params.") : (return redirect_to request.referer)
-    end
+      if params[:amount].blank? || params[:user_uid].blank?
+        request.referer.blank? ? (return render :failed, locals: { error_info: "ERROR: lack of params." } ) : (return redirect_to request.referer)
+      end
 
-  	order = StraightServerKit::Order.new(amount: params[:amount] || 1, callback_data: params[:order_uid] || 1, keychain_id: last_keychain_id + 1)
-    @order = @client.orders.create(order)
-    @amount = params[:amount].to_f
-    render :invoice and return
+      order = StraightServerKit::Order.new(amount: params[:amount], callback_data: params[:uid], keychain_id: last_keychain_id + 1)
+      @order = @client.orders.create(order)
+      @amount = params[:amount].to_f
+      return render :success
+    rescue
+      return render :failed, locals: { error_info: "ERROR: failed to connect to straight server. Please try again later." }
+    end
   end
 
   def payment_callback
