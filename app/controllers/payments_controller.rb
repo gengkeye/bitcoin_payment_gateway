5e278ef4b9768e10405b340de58380d6f5406b58
oldseven <oldseven@nn.com>
2017-03-30 15:01:18 +0800

    add countdown function

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -22,22 +22,20 @@ class PaymentsController < ApplicationController
   end
 
   def create
+    if params[:amount].blank? || params[:uid].blank?
+      return render :failed, locals: { error_info: "ERROR: lack of params." }
+    end
     begin
       # res = HTTParty.get(ENV['STRAIGHT_SERVER_URL'] + 'gateways/1/last_keychain_id')
       # res = Excon.get(ENV['STRAIGHT_SERVER_URL'] + 'gateways/1/last_keychain_id')[:body]
       # last_keychain_id = JSON.parse(res)['last_keychain_id'].to_i
-
-      if params[:amount].blank? || params[:uid].blank?
-        return render :failed, locals: { error_info: "ERROR: lack of params." }
-      end
-
       order = StraightServerKit::Order.new(amount: params[:amount], callback_data: params[:uid]) # , keychain_id: last_keychain_id + 1
       @order = @client.orders.create(order)
-      @amount = params[:amount].to_f
-      return render :success
     rescue
       return render :failed, locals: { error_info: "ERROR: failed to connect to straight server. Please try again later." }
     end
+    @amount = params[:amount].to_f
+    return render :success
   end
 
   def payment_callback
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,7 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( countdown.js )
+Rails.application.config.assets.precompile += %w(
+    jquery.plugin.js
+	jquery.countdown.css
+	jquery.countdown.js)
