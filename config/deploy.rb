1f34fdc911dfcc3183de4b2b2678cad4e03054d4
oldseven <oldseven@nn.com>
2017-04-12 17:06:54 +0800

    fuck can't start puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,18 +37,6 @@ set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets }
 # in order to prevent bundler from overwriting the version controlled binstubs
 set :bundle_binstubs, nil
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      # execute "mkdir #{shared_path}/tmp/sockets -p"
-      # execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
@@ -64,24 +52,20 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      # before 'deploy:restart', 'puma:start'
+      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
   end
 
-  # desc 'Restart application'
-  # task :restart do
-  #   on roles(:app), in: :sequence, wait: 5 do
-  #     invoke 'puma:restart'
-  #   end
-  # end
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
 
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
 end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
