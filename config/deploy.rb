d157f4cef6137f8c11f87cda558668ae18b1adca
oldseven <oldseven@nn.com>
2017-04-12 17:51:44 +0800

    modify nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,10 +19,11 @@ set :deploy_via,      :remote_cache
 # set :puma_preload_app, true
 # set :puma_worker_timeout, nil
 
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 # puma
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :puma_role, :app
 set :puma_config_file, 'config/puma-web.rb'
+
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
@@ -37,35 +38,37 @@ set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets }
 # in order to prevent bundler from overwriting the version controlled binstubs
 set :bundle_binstubs, nil
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
+# namespace :deploy do
+#   desc "Make sure local git is in sync with remote."
+#   task :check_revision do
+#     on roles(:app) do
+#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#         puts "WARNING: HEAD is not the same as origin/master"
+#         puts "Run `git push` to sync changes."
+#         exit
+#       end
+#     end
+#   end
 
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
+#   desc 'Initial Deploy'
+#   task :initial do
+#     on roles(:app) do
+#       before 'deploy:restart', 'puma:start'
+#       invoke 'deploy'
+#     end
+#   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       invoke 'puma:restart'
+#     end
+#   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
-end
+#   before :starting,     :check_revision
+#   after  :finishing,    :compile_assets
+#   after  :finishing,    :cleanup
+#   after  :finishing,    :restart
+# end
+
+after 'deploy:publishing', 'deploy:restart'
