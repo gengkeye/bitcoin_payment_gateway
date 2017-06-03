d8053cd004831bbdabbea0bb905f12d45e28e8db
oldseven <oldseven@nn.com>
2017-06-03 14:24:22 +0800

    remove redundant file

--- app/workers/ltc_price_worker.rb
+++ app/workers/ltc_price_worker.rb
@@ -8,11 +8,11 @@ class LtcPriceWorker
 				Rails.logger.debug "Don't do any thing now. Time: #{Time.now}"
 			when 1
 				Rails.logger.debug "You should sell some ltc now. Time: #{Time.now}"
-				SendMailWorker.perform_async("You should sell some ltc now. Time: #{Time.now}")
+	            LtcMonitorMailer.suggest_mail("You should sell some ltc now. Time: #{Time.now}").deliver_later
 				sleep 60
 			when 2
 				Rails.logger.debug "You should buy some ltc now. Time: #{Time.now}"
-				SendMailWorker.perform_async("You should sell some ltc now. Time: #{Time.now}")
+	            LtcMonitorMailer.suggest_mail("You should buy some ltc now. Time: #{Time.now}").deliver_later
 				sleep 60
 			end
 			sleep 30
--- app/workers/send_mail_worker.rb
+++
@@ -1,7 +0,0 @@
-class SendMailWorker
-  include Sidekiq::Worker
-
-  def perform(content)
-	LtcMonitorMailer.suggest_mail(content).deliver_now
-  end
-end
