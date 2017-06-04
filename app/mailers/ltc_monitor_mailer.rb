d47e0bdcbb57d566f7226d1b848f16b67e3d3a80
oldseven <oldseven@nn.com>
2017-06-04 16:48:31 +0800

    should pass an id but not an object as the params

--- app/mailers/ltc_monitor_mailer.rb
+++ app/mailers/ltc_monitor_mailer.rb
@@ -1,6 +1,7 @@
 class LtcMonitorMailer < ApplicationMailer
-	def suggest_mail(content)
-		@content = content
+	def suggest_mail(email_id)
+		@content = SuggestionEmail.find_by(email_id)
+		raise "Email not found." if @content.nil?
 		mail(to: ['dejiegeng@gmail.com', 'steven.c.j.huang@gmail.com'], subject: 'Suggestion From Jesus', date: Time.now) do |format|
 			format.text
 			format.html
--- app/workers/ltc_price_worker.rb
+++ app/workers/ltc_price_worker.rb
@@ -9,7 +9,7 @@ class LtcPriceWorker
 			case params[:sugguestion]
 			when 0, 1
 			    Rails.logger.debug("You should" + SuggestionEmail.suggestions.key(params[:sugguestion]) + "some ltc now. Time: #{Time.now}")
-	            LtcMonitorMailer.suggest_mail(@suggestion_email).deliver_later
+	            LtcMonitorMailer.suggest_mail(@suggestion_email.id).deliver_later
 				sleep 120
 			when 2
 				Rails.logger.debug "Don't do any thing now. Time: #{Time.now}"
