6cdd0598930a30e548202a9eb5e29855b8c5ed34
oldseven <oldseven@nn.com>
2017-06-04 18:26:35 +0800

    update_nums

--- app/models/suggestion_email.rb
+++ app/models/suggestion_email.rb
@@ -12,5 +12,14 @@ class SuggestionEmail < ApplicationRecord
 		keep: 2
 	}
    def update_nums
+   		@last2_email = SuggestionEmail.find_by(id: self.id - 2)
+	 	if @last_email = SuggestionEmail.find_by(id: self.id - 1)
+	 		if (@last_email.suggestion == 'buy' && self.last_price > @last_email.last_price) * 1.01 \
+	 			|| (@last_email.suggestion == 'sell' && self.last_price < @last_email.last_price / 1.01 )
+	 			@last_email.update(correct: true, correct_num: @last2_email&.correct_num.to_i + 1)
+	 		else
+	 			@last_email.update(correct: false, incorrect_num: @last2_email&.incorrect_num.to_i + 1)
+	 		end
+	 	end
    end
 end
