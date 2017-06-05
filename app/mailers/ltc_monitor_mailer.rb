9d70b9ff436f780a99a1bb742fefdb347fc91172
oldseven <oldseven@nn.com>
2017-06-05 11:34:35 +0800

    fix error: + for nil

--- app/mailers/ltc_monitor_mailer.rb
+++ app/mailers/ltc_monitor_mailer.rb
@@ -1,10 +1,12 @@
 class LtcMonitorMailer < ApplicationMailer
 	def suggest_mail(email_id)
 		@suggestion_email = SuggestionEmail.find_by(id: email_id)
+		@last_email = SuggestionEmail.find_by(id: email_id - 1)
 		raise "Email not found." if @suggestion_email.nil?
-		@correct_nums = @suggestion_email.correct_num
-		@incorrect_nums = @suggestion_email.incorrect_num
-		@success_rate = @correct_nums.to_f / (@correct_nums + @incorrect_nums)
+		@correct_nums = @last_email.correct_num.to_i
+		@incorrect_nums = @last_email.incorrect_num.to_i
+		total_nums = @correct_nums + @incorrect_nums
+		@success_rate = (total_nums == 0 ? 0 : (@correct_nums.to_f / total_nums))
 		mail(to: %w(dejiegeng@gmail.com steven.c.j.huang@gmail.com),
 			 subject: 'Suggestion From Jesus',
 			 date: Time.now) do |format|
